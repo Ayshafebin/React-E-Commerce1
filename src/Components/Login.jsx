d7113e7a745d19cacd159a2d4bb74a25b0dd80f3
Ayshafebin <febinayisha0@gmail.com>
2024-02-22 15:00:46 +0530

    New updation

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Shoe.css";
 import { FaWordpress } from "react-icons/fa";
 import { Link } from "react-router-dom";
-// import { MyContext } from "./Context";
+import { MyContext } from "./Context";
 import { useDispatch, useSelector } from "react-redux";
 import { loginSuccess } from "../Redux/AuthSlice";
 
 const Login =  () => {
-  // const { signup, setLogin, setUsername } = useContext(MyContext);
+  const { signup, setLogin, setUsername } = useContext(MyContext);
   const mynav = useNavigate();
   const isLogin=useSelector(state=>state.auth.isAuthenticated)
  
